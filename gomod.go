1f38c34351cb7175f49b505d738e12b506d9dbb4
Masaaki Goshima <goccy54@gmail.com>
2019-12-24 10:56:21 +0900

    Add gomod.go

---
+++ gomod.go
@@ -0,0 +1,54 @@
+package rebirth
+
+import (
+	"bytes"
+	"os"
+	"strconv"
+)
+
+const goModPath = "go.mod"
+
+func existsGoMod() bool {
+	_, err := os.Stat(goModPath)
+	return err == nil
+}
+
+var (
+	slashSlash = []byte("//")
+	moduleStr  = []byte("module")
+)
+
+// this original source is cmd/go/internal/modfile/read.go
+func parseModulePath(mod []byte) string {
+	for len(mod) > 0 {
+		line := mod
+		mod = nil
+		if i := bytes.IndexByte(line, '\n'); i >= 0 {
+			line, mod = line[:i], line[i+1:]
+		}
+		if i := bytes.Index(line, slashSlash); i >= 0 {
+			line = line[:i]
+		}
+		line = bytes.TrimSpace(line)
+		if !bytes.HasPrefix(line, moduleStr) {
+			continue
+		}
+		line = line[len(moduleStr):]
+		n := len(line)
+		line = bytes.TrimSpace(line)
+		if len(line) == n || len(line) == 0 {
+			continue
+		}
+
+		if line[0] == '"' || line[0] == '`' {
+			p, err := strconv.Unquote(string(line))
+			if err != nil {
+				return "" // malformed quoted string or multiline module path
+			}
+			return p
+		}
+
+		return string(line)
+	}
+	return "" // missing module path
+}
